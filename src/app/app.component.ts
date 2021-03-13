388acfb1b91603d7b38a6ee663a56f291731bc1c
Dawit Aseged <43924962+Dawit-Aseged@users.noreply.github.com>
2021-03-13 13:12:34 +0300

    Changed the name of the project
    Changed the name of the project by changing the title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reminder';
+  title = 'MAD Notes';
 
   expanded = false;
   toggle() {
