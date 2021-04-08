ffe9923a99c89ab9b8d4fb7fa7fcabf67232dc5a
Dawit Aseged <43924962+Dawit-Aseged@users.noreply.github.com>
2021-04-08 16:14:59 +0300

    Added multiple todos
    When "Enter" is pressed, a new todo will appear on the bottom

--- src/app/notes/note-create/note-create.component.ts
+++ src/app/notes/note-create/note-create.component.ts
@@ -1,5 +1,15 @@
-import { Component, Inject, EventEmitter } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {
+  Component,
+  Inject,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
 
 export interface DialogData {
@@ -8,33 +18,40 @@ export interface DialogData {
 }
 
 @Component({
-    selector: 'app-note-create',
-    templateUrl: './note-create.component.html',
-    styleUrls: ['./note-create.component.css']
+  selector: 'app-note-create',
+  templateUrl: './note-create.component.html',
+  styleUrls: ['./note-create.component.css'],
 })
 export class NoteCreateComponent {
-
   noteOptions = [
-    {type: "Note", id: 1},
-    {type: "Todo", id: 2}
-  ]
+    { type: 'Note', id: 1 },
+    { type: 'Todo', id: 2 },
+  ];
+
+  // This is the array that temporarily holds the todos. It is initialized as an empty todo
+  todoList = [{ content: '', checked: false }];
   isTodo = false;
   constructor(
     public dialogRef: MatDialogRef<NoteCreateComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
 
+  // The following occurs when the button "No Thanks" is pressed
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  checkChanged(input: HTMLInputElement) {
-    input.classList.toggle("strikethrough")
+  // This occurs when the user changes the type of note (Either Note or Todo)
+  selectChanged(event: MatSelectChange) {
+    if (event.value == 1) this.isTodo = false;
+    else this.isTodo = true;
   }
 
-  selectChanged(event: MatSelectChange) {
-    if(event.value == 1)
-      this.isTodo = false;
-    else
-      this.isTodo = true;
+  //This creates a new todo when enter is clicked
+  enterClicked() {
+    this.todoList.push({
+      content: '',
+      checked: false,
+    });
   }
 }
